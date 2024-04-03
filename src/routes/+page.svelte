adc7b2e7cc835497b32a8b9267a7d61d8c381079
Jesus-Cries <16zeichen@gmail.com>
2024-04-03 23:18:21 +0200

    Create basic feature page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import { fade } from "svelte/transition"
     import Hero from "./homepage/Hero.svelte"
+    import Features from "./homepage/Features.svelte"
 </script>
 
-<div transition:fade={{ duration: 100 }} class="absolute">
+<div transition:fade={{ duration: 100 }} class="absolute w-screen">
     <Hero />
+    <Features />
 </div>
--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -14,7 +14,7 @@
 
 <div class="flex items-center justify-between pb-9 pl-4 pr-4 pt-0 md:pl-7 md:pr-10 md:pt-1">
     <Domain />
-    <div class="flex pt-6 md:gap-14">
+    <div class="flex -translate-y-1 pt-6 md:gap-14">
         {#each pages as page}
             <a
                 class="btn btn-ghost no-animation btn-xs uppercase md:btn-md hover:bg-transparent hover:text-primary"
