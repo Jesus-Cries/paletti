1249e5c5f67be9eb6fe1a298c71786487fd3d72d
Jesus-Cries <16zeichen@gmail.com>
2024-04-03 22:05:34 +0200

    Fix bug regarding undefined palette

--- src/routes/Domain.svelte
+++ src/routes/Domain.svelte
@@ -7,9 +7,15 @@
 <a class="flex h-fit w-fit content-center items-baseline gap-1.5 md:gap-4" href="/">
     <Logo
         colors={[
-            $page.url.pathname === "/palettes" ? $palettes[$page.data.focusedPalette][1] : "6F0021",
-            $page.url.pathname === "/palettes" ? $palettes[$page.data.focusedPalette][4] : "EF347C",
-            $page.url.pathname === "/palettes" ? $palettes[$page.data.focusedPalette][7] : "FFB9CE",
+            $page.url.pathname === "/palettes" && $palettes[$page.data.focusedPalette] !== undefined
+                ? $palettes[$page.data.focusedPalette][1]
+                : "6F0021",
+            $page.url.pathname === "/palettes" && $palettes[$page.data.focusedPalette] !== undefined
+                ? $palettes[$page.data.focusedPalette][4]
+                : "EF347C",
+            $page.url.pathname === "/palettes" && $palettes[$page.data.focusedPalette] !== undefined
+                ? $palettes[$page.data.focusedPalette][7]
+                : "FFB9CE",
         ]}
         classOverride="md:scale-100 scale-75"
     />
