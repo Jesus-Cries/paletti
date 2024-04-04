4fa158bdbd2d8e6a7de1a0c83b1ecf539075ebd6
Jesus-Cries <16zeichen@gmail.com>
2024-04-04 23:54:58 +0200

    Give Hero more space to breathe

--- src/routes/homepage/Hero.svelte
+++ src/routes/homepage/Hero.svelte
@@ -2,7 +2,7 @@
     import Logo from "../Logo.svelte"
 </script>
 
-<div class="flex flex-col items-center gap-12 pt-6 md:gap-20">
+<div class="flex flex-col items-center gap-12 pb-12 pt-16 md:gap-20 md:pb-8 md:pt-20">
     <Logo classOverride="scale-[1.5] md:scale-[1.9]" />
     <div
         class="flex flex-col items-center gap-4 text-3xl font-medium tracking-wider text-gray-600 md:text-5xl"
