2c55cc0e36cee1773dba6fe4bf0e8aef9b94763f
Jesus-Cries <16zeichen@gmail.com>
2024-04-18 17:48:11 +0200

    Fix Logo in Domain not reliably changing color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,6 @@
 
     // TODO: Make hero fancier (highlight some of the text)
     // TODO:? Add github link
-    // FIXME: Navbar logo doesn't change color if reloading on homepage and navigating to palettes
 </script>
 
 <div transition:fade={{ duration: 100 }} class="absolute w-full overflow-x-hidden">
--- src/routes/Domain.svelte
+++ src/routes/Domain.svelte
@@ -2,26 +2,28 @@
     import { page } from "$app/stores"
     import { palettes } from "./palettes/store"
     import Logo from "./Logo.svelte"
+
+    let colors: string[] = []
+
+    $: if (
+        ["/palettes", "/palettes/"].includes($page.url.pathname) &&
+        $palettes[$page.data.focusedPalette]
+    ) {
+        colors = [
+            $palettes[$page.data.focusedPalette].colors[1],
+            $palettes[$page.data.focusedPalette].colors[4],
+            $palettes[$page.data.focusedPalette].colors[7],
+        ]
+    } else {
+        colors = ["6F0021", "EF347C", "FFB9CE"]
+    }
 </script>
 
 <a
     class="flex h-fit w-fit -translate-y-1.5 content-center items-baseline gap-3 md:-translate-y-2 md:gap-4"
     href="/"
 >
-    <Logo
-        colors={[
-            $page.url.pathname === "/palettes" && $palettes[$page.data.focusedPalette] !== undefined
-                ? $palettes[$page.data.focusedPalette].colors[1]
-                : "6F0021",
-            $page.url.pathname === "/palettes" && $palettes[$page.data.focusedPalette] !== undefined
-                ? $palettes[$page.data.focusedPalette].colors[4]
-                : "EF347C",
-            $page.url.pathname === "/palettes" && $palettes[$page.data.focusedPalette] !== undefined
-                ? $palettes[$page.data.focusedPalette].colors[7]
-                : "FFB9CE",
-        ]}
-        classOverride="scale-[0.6] md:scale-75"
-    />
+    <Logo {colors} classOverride="scale-[0.6] md:scale-75" />
     <p class="comfortaa none -translate-y-2 text-xl font-bold md:-translate-y-1.5 md:text-2xl">
         paletti
     </p>
