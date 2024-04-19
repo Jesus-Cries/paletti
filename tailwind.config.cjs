2a9320a516fe694da7c3325d7fafd2ac569b6ffb
Jesus-Cries <16zeichen@gmail.com>
2024-04-20 01:45:28 +0200

    Refactor tailwind colors into object notation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,27 +4,31 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                // Gray
-                "gray-900": "#0F1720",
-                "gray-800": "#222933",
-                "gray-700": "#383D47",
-                "gray-600": "#575B66",
-                "gray-500": "#747883",
-                "gray-400": "#90949E",
-                "gray-300": "#C1C5CD",
-                "gray-200": "#E7EAEF",
-                "gray-100": "#F7F8FB",
+                gray: {
+                    900: "#0F1720",
+                    800: "#222933",
+                    700: "#383D47",
+                    600: "#575B66",
+                    500: "#747883",
+                    DEFAULT: "#747883",
+                    400: "#90949E",
+                    300: "#C1C5CD",
+                    200: "#E7EAEF",
+                    100: "#F7F8FB",
+                },
 
-                // Primary
-                "primary-900": "#330008",
-                "primary-800": "#6D021F",
-                "primary-700": "#A4063A",
-                "primary-600": "#DA0D57",
-                "primary-500": "#EF347C",
-                "primary-400": "#F55F95",
-                "primary-300": "#FB8CB0",
-                "primary-200": "#FEBACC",
-                "primary-100": "#FFEBEE",
+                primary: {
+                    900: "#330008",
+                    800: "#6D021F",
+                    700: "#A4063A",
+                    600: "#DA0D57",
+                    500: "#EF347C",
+                    DEFAULT: "#EF347C",
+                    400: "#F55F95",
+                    300: "#FB8CB0",
+                    200: "#FEBACC",
+                    100: "#FFEBEE",
+                },
             },
             screens: {
                 xs: "500px",
