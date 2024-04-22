36acdbe66fed127d1f5623b3ed13ac0dcded7a8b
Jesus-Cries <16zeichen@gmail.com>
2024-04-22 11:26:12 +0200

    Extract colors into own file

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,15 @@
+import { colors } from "./src/lib/colors"
+
+const gray = colors.gray
+const primary = colors.primary
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{html,js,svelte,ts}"],
     darkMode: ["selector", "[data-theme='dark']"],
     theme: {
         extend: {
-            colors: {
-                gray: {
-                    900: "#0D0D12",
-                    800: "#242530",
-                    700: "#3D404D",
-                    600: "#575B68",
-                    500: "#737782",
-                    DEFAULT: "#737782",
-                    400: "#9094A2",
-                    300: "#AFB2C0",
-                    200: "#D0D1DC",
-                    100: "#F3F3F7",
-                },
-                primary: {
-                    900: "#1F0007",
-                    800: "#60001D",
-                    700: "#A10037",
-                    600: "#D90855",
-                    500: "#EF347C",
-                    DEFAULT: "#EF347C",
-                    400: "#F95C95",
-                    300: "#FF87B0",
-                    200: "#FFB9CE",
-                    100: "#FFEBEF",
-                },
-            },
+            colors,
             screens: {
                 xs: "500px",
             },
