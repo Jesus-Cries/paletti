d587f344084f7fae7c5e266878f052a8d87e7bf9
Jesus-Cries <16zeichen@gmail.com>
2024-05-13 23:38:58 +0200

    Update palette config with new adapted color palette

--- src/routes/palettes/+page.ts
+++ src/routes/palettes/+page.ts
@@ -3,7 +3,7 @@ export const ssr = false
 export const load = async ({ url }) => {
     return {
         names: url.searchParams.get("names")?.split(",") || ["Primary"],
-        mainColors: url.searchParams.get("baseColors")?.split(",") || ["EF347C"],
+        mainColors: url.searchParams.get("baseColors")?.split(",") || ["E82C74"],
         hueRotations: (url.searchParams.get("hueRotations")?.split(",") || ["0"]).map((n) =>
             parseInt(n)
         ),
